63c70b506bbf833d77faac2b8ef045c0c70117a9
EdwinRy <edwinrybarczyk4d@gmail.com>
2019-07-12 16:19:28 +0100

    Add project files.

---
+++ RockPaperScissorsWithFriends/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RockPaperScissorsWithFriends.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
